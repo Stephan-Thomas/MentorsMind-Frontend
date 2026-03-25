c2ba282b7b783a31bb9de5cd354e88f1c850bb64
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-25 03:56:41 +0100

    add interceptor for jwt, refresh and retry logic

--- src/services/api.client.ts
+++ src/services/api.client.ts
@@ -1,4 +1,4 @@
-import type { AxiosInstance } from "axios";
+import type { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
 import axios from "axios";
 
 let accessToken: string;
@@ -10,6 +10,7 @@ export const setTokens = (access: string, refresh: string) => {
 };
 
 const api: AxiosInstance = axios.create({ baseURL: "/api", timeout: 10000 });
+const MAX_RETRIES = 3;
 
 api.interceptors.request.use((config) => {
   if (accessToken && config.headers) {
@@ -19,4 +20,42 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  async (err: AxiosError) => {
+    const originalReq = err.config as AxiosRequestConfig & { _retry?: number };
+
+    // Transient error retry
+    if (!originalReq._retry) {
+      originalReq._retry = 0;
+    }
+
+    if (
+      err.response?.status &&
+      err.response.status >= 500 &&
+      originalReq._retry < MAX_RETRIES
+    ) {
+      originalReq._retry++;
+      return api(originalReq);
+    }
+
+    // Refresh token
+    if (err.response?.status === 401 && refreshToken) {
+      try {
+        const res = await axios.post("/auth/refesh", { refreshToken });
+        setTokens(res.data.accessToken, res.data.refreshToken);
+
+        if (originalReq.headers) {
+          originalReq.headers.Authorization = `Bearer ${res.data.accessToken}`;
+        }
+
+        return api(originalReq);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(err);
+  },
+);
 export default api;
